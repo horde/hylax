103993dd6bea9ae1a16185ee83cb1ad9662053a0
Jan Schneider <jan@horde.org>
2011-02-11 19:07:33 +0100

    Revamp configuration file layout.
    I changed my mind. I'm convinced now that it makes more sense to provide
    default configuration files instead of .dist versions that have to be
    copied for each installation. This makes at least sense for any
    configuration files that 99% of the people won't change from the
    defaults anyway. Instead load *.local.php versions of those
    configuration files if they exist. This makes both installation and
    upgrading much easier.
    
    I left those .dist files untouched that either *must* be edited to make
    any sense, or that are optional and just examples (menu.php.dist,
    hooks.php.dist).
    
    I'm still considering to do the same with the backends.php files.
    Providing sane defaults and adding a configuration option to pick from
    one of the shipped examples might make sense in most cases.

--- config/prefs.php
+++ config/prefs.php
@@ -2,7 +2,9 @@
 /**
  * See horde/config/prefs.php for documentation on the structure of this file.
  *
- * $Id$
+ * IMPORTANT: Local overrides should be placed in prefs.local.php, or
+ * prefs-servername.php if the 'vhosts' setting has been enabled in Horde's
+ * configuration.
  */
 
 $prefGroups['display'] = array(
@@ -22,3 +24,8 @@ $_prefs['hylax_default_view'] = array(
                     'compose' => _("Compose")),
     'desc' => _("Select the view to display after login:")
 );
+
+/* Local overrides. */
+if (file_exists(dirname(__FILE__) . '/prefs.local.php')) {
+    include dirname(__FILE__) . '/prefs.local.php';
+}
